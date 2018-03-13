6d54b9593b245959eeb277f60678f2888a046708
kfrn <knfrances@gmail.com>
2018-03-13 20:57:45 +1300

    Add images; amend HTML; improve styling

--- src/View.elm
+++ src/View.elm
@@ -1,8 +1,8 @@
 module View exposing (view)
 
-import AspectRatios exposing (allAspectRatios, aspectRatioDisplayName)
-import Html exposing (Html, div, em, h1, i, input, nav, p, span, text)
-import Html.Attributes exposing (attribute, class, id, placeholder, type_, value)
+import AspectRatios exposing (AspectRatio(..), allAspectRatios, aspectRatioDisplayName)
+import Html exposing (Html, div, em, h1, i, img, input, label, nav, p, span, text)
+import Html.Attributes exposing (attribute, class, id, placeholder, src, type_, value)
 import Html.Events exposing (onInput)
 import Links exposing (LinkName(..), link)
 import Model exposing (Model)
@@ -33,7 +33,7 @@ navbar =
                     ]
                 ]
             , div [ class "navbar-item app-tagline" ]
-                [ p [ class "is-size-6" ] [ em [] [ text "calculate frame size here!" ] ]
+                [ p [ class "is-size-6" ] [ em [] [ text "calculate frame sizes" ] ]
                 ]
             ]
         ]
@@ -51,19 +51,50 @@ pageContent : Model -> Html Msg
 pageContent model =
     div [ id "main-content" ]
         [ calculator model
-        , div [] [ text "image will go here" ]
+        , referenceImage model.aspectRatio
+        , div [] [ text <| "current width is: " ++ toString model.width ]
+        , div [] [ text <| "current height is: " ++ toString model.height ]
         ]
 
 
 calculator : Model -> Html Msg
 calculator model =
     div [ id "calculator" ]
-        [ renderSelect model.aspectRatio ChangeAspectRatio aspectRatioDisplayName allAspectRatios
-        , createInput model.width UpdateWidth
-        , createInput model.height UpdateHeight
-        , div [] [ text <| "current AR is: " ++ toString model.aspectRatio ]
-        , div [] [ text <| "current width is: " ++ toString model.width ]
-        , div [] [ text <| "current height is: " ++ toString model.height ]
+        [ selectAspectRatio model.aspectRatio
+        , inputFields model
+        ]
+
+
+selectAspectRatio : AspectRatio -> Html Msg
+selectAspectRatio currentAR =
+    div [ id "select-aspect-ratio" ]
+        [ div [ class "field is-horizontal" ]
+            [ div [ class "field-label is-normal" ]
+                [ label [] [ text "Choose aspect ratio: " ]
+                ]
+            , div [ class "field-body" ]
+                [ renderSelect currentAR ChangeAspectRatio aspectRatioDisplayName allAspectRatios ]
+            ]
+        ]
+
+
+inputFields : Model -> Html Msg
+inputFields model =
+    div [ id "number-inputs" ]
+        [ div [ class "field is-horizontal" ]
+            [ div [ class "field-label is-normal" ]
+                [ label [] [ text "Height: " ]
+                ]
+            , div [ class "field-body" ]
+                [ createInput model.width UpdateWidth ]
+            ]
+        , div [ class "field is-horizontal" ]
+            [ div [ class "field-label is-normal" ]
+                [ label [] [ text "Width: " ]
+                ]
+            , div [ class "field-body" ]
+                [ createInput model.height UpdateHeight ]
+            ]
         ]
 
 
@@ -71,7 +102,6 @@ createInput : Float -> (String -> Msg) -> Html Msg
 createInput val message =
     let
         roundNumber r =
-            -- TODO: improve this? There must be a method to check this
             r == (toFloat <| floor r)
 
         numForDisplay =
@@ -89,3 +119,34 @@ createInput val message =
         -- , value numForDisplay
         ]
         []
+
+
+referenceImage : AspectRatio -> Html Msg
+referenceImage ratio =
+    let
+        imgSrc =
+            "/images/prince_guitar_" ++ namePart ++ ".jpg"
+
+        namePart =
+            case ratio of
+                Academy ->
+                    "Academy"
+
+                SD ->
+                    "4_3"
+
+                HD ->
+                    "16_9"
+
+                WidescreenEU ->
+                    "5_3"
+
+                Widescreen185 ->
+                    "1-85_1"
+
+                Widescreen239 ->
+                    "Scope_2-39_1"
+    in
+    div [ id "reference-image" ]
+        [ img [ src imgSrc ] []
+        ]
